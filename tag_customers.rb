65219ed53f42e39f2894bba6f042af62869f7cb6
Eric Davis <edavis@littlestreamsoftware.com>
2016-03-31 15:59:16 -0700

    Test the connection to the Shopify API

--- tag_customers.rb
+++ tag_customers.rb
@@ -1,7 +1,20 @@
 #!/usr/bin/env ruby
+require 'pry'
+require 'shopify_api'
 require 'dotenv'
 Dotenv.load
 
 class TagCustomers
+  attr_accessor :shop_url
 
+  # Configure the Shopify connection
+  def initialize
+    @shop_url = "https://#{ENV["SHOPIFY_API_KEY"]}:#{ENV["SHOPIFY_PASSWORD"]}@#{ENV["SHOP"]}.myshopify.com/admin"
+    ShopifyAPI::Base.site = @shop_url
+  end
+
+  # Tests the Shopify connection with a simple GET request
+  def test_connection
+    return ShopifyAPI::Shop.current
+  end
 end
--- test/tag_customers_test.rb
+++ test/tag_customers_test.rb
@@ -10,4 +10,10 @@ class TestTagCustomers < Minitest::Test
   def test_loaded_file
     assert TagCustomers
   end
+
+  def test_connection
+    connection_test = TagCustomers.new.test_connection
+    assert connection_test
+    assert_equal "#{ENV['SHOP']}.myshopify.com", connection_test.domain
+  end
 end
